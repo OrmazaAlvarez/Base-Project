27e97a7694df737e6d1c8ab51bd77af7255940f1
Lenin Antonio Ormaza Alvarez <lenin.antonio.ormaza12@hotmail.com>
2021-01-05 21:32:18 -0500

    Capturando eventos del teclado de forma global y previniendo fugas de memorias por este tipo de eventos

--- src/app/shopping-cart-status/shopping-cart-status.component.ts
+++ src/app/shopping-cart-status/shopping-cart-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Product } from '../interfaces/product';
 
 @Component({
@@ -6,17 +6,19 @@ import { Product } from '../interfaces/product';
   templateUrl: './shopping-cart-status.component.html',
   styleUrls: ['./shopping-cart-status.component.sass']
 })
-export class ShoppingCartStatusComponent implements OnInit, OnChanges {
+export class ShoppingCartStatusComponent implements OnInit, OnChanges, OnDestroy {
   @Input() total: number;
   @Input() shoppingCart: Array<Product>;
-
   @Output() pay: EventEmitter<null> = new EventEmitter();
+
   constructor() { 
     this.shoppingCart = new Array<Product>();
     this.total = 0;
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.triggerKeyboardEvent();
+  }
 
    ngOnChanges(changes: SimpleChanges) {
     if(changes.total.currentValue < changes.total.previousValue){
@@ -24,7 +26,21 @@ export class ShoppingCartStatusComponent implements OnInit, OnChanges {
     }
   }
 
+  ngOnDestroy(){
+    document.removeEventListener("keypress", this.onKeyboard)   
+  }
+
   makePayment(){
     this.pay.emit();
+  }
+
+  onKeyboard(_event){
+    if(_event.code === "Enter" || _event.code === "NumpadEnter"){
+      this.makePayment();
+    }
+  }
+
+  triggerKeyboardEvent(){
+    document.addEventListener("keypress", (evento) => {this.onKeyboard(evento);});
   }
 }
